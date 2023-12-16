c93becbc16eff33d8b3b3bb6e4ce72472bfe9a79
githubscientist <sathish.datascientist@gmail.com>
2023-12-16 17:27:22 +0530

    start the express server

---
+++ app.js
@@ -0,0 +1,8 @@
+// require express
+const express = require('express');
+
+// create a new express app
+const app = express();
+
+// export the app
+module.exports = app;
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // import mongoose
 const mongoose = require('mongoose');
 const config = require('./utils/config');
+const app = require('./app');
 
 console.log('Connecting to MongoDB...');
 // connect to mongodb
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB...');
+
+        // start the server
+        app.listen(config.PORT, () => {
+            console.log(`Server running on port ${config.PORT}`);
+        });
     })
     .catch((error) => {
         console.log('Error connecting to MongoDB:', error.message);
